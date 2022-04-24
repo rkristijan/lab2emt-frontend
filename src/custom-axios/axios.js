b368bc8f5e111185b420b34108455a78ca3b2050
Kristijan <ristovski55@yahoo.com>
2022-04-24 20:34:43 +0200

    routing fix

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9091',
+    baseURL: 'https://library-lab2emt-backend.herokuapp.com',
     headers: {
         'Access-Control-Allow-Origin' : '*'
     }
